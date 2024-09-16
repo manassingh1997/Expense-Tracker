516352266ff74c9c08fabc52e5dfc3b7ec55b671
Manas Singh <manassinghsolanki@hotmail.com>
2024-09-16 11:05:13 +0530

    Updating database to PostgreSQL

--- expencetracker/settings.py
+++ expencetracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'expencetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expense_tracker_db',
+        'USER' : 'postgres',
+        'password': 'admin',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
